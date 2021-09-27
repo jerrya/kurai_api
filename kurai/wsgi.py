f21b6a0d7581bc42dd4215fad2b1c31c5414f540
Jerry Alex <jerryalex08@gmail.com>
2021-09-26 21:35:33 -0500

    Renaming

--- kurai/wsgi.py
+++ kurai/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tokendrop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kurai.settings')
 
 application = get_wsgi_application()
