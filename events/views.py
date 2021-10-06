118ceaf71f7d67d2b856933af91281e697c74f9a
Angelo Wong <angelowong@angelos-air.myfiosgateway.com>
2021-10-05 21:23:33 -0400

    wrap api requests in do while loop to get all possible transactions

--- events/views.py
+++ events/views.py
@@ -6,9 +6,10 @@ import requests
 
 from events.models import Event
 from events.serializers import EventSerializer
+import time
 
 WEI = 10 ** 18
-
+API_PAGINATION_LIMIT = 300
 
 class RetrieveEvents(APIView):
     def get(self, request, slug, format=None):
@@ -20,34 +21,41 @@ class RetrieveEvents(APIView):
             # ^ dependent on timestamp in database and use it as part of the occurred_before value
             data = []
             url = "https://api.opensea.io/api/v1/events"
-            querystring = {
-                "collection_slug": slug,
-                "event_type": "successful",
-                "only_opensea": "true",
-                "offset": "0",
-                "limit": "10",
-                # "occurred_after": ""
-            }
-            headers = {"Accept": "application/json"}
             i = 0
-            response = requests.request("GET", url, headers=headers, params=querystring)
-            json = response.json()["asset_events"]
-            if response.status_code == status.HTTP_200_OK and len(json) > 0:
-                for trade in json:
-                    if trade["transaction"]["timestamp"] is None:
-                        continue
-                    e = Event.objects.create(
-                        collection_slug=trade["asset"]["collection"]["slug"],
-                        buyer_address=trade["winner_account"]["address"],
-                        seller_address=trade["seller"]["address"],
-                        contract_address=trade["asset"]["asset_contract"]["address"],
-                        price=trade["total_price"],
-                        timestamp=trade["transaction"]["timestamp"],
-                        token_id=trade["asset"]["token_id"],
-                        transaction_hash=trade["transaction"]["transaction_hash"],
-                        event_type=trade["event_type"]
-                    )
-                    data.append(e)
+            while True:
+                querystring = {
+                    "collection_slug": slug,
+                    "event_type": "successful",
+                    "only_opensea": "true",
+                    "offset": str(i*API_PAGINATION_LIMIT),
+                    "limit": str(API_PAGINATION_LIMIT),
+                    # "occurred_after": "" # TODO
+                }
+                headers = {"Accept": "application/json"}
+
+                response = requests.request("GET", url, headers=headers, params=querystring)
+                json = response.json()["asset_events"]
+                if response.status_code == status.HTTP_200_OK and len(json) > 0:
+                    for trade in json:
+                        if trade["transaction"]["timestamp"] is None:
+                            continue
+                        e = Event.objects.create(
+                            collection_slug=trade["asset"]["collection"]["slug"],
+                            buyer_address=trade["winner_account"]["address"],
+                            seller_address=trade["seller"]["address"],
+                            contract_address=trade["asset"]["asset_contract"]["address"],
+                            price=trade["total_price"],
+                            timestamp=trade["transaction"]["timestamp"],
+                            token_id=trade["asset"]["token_id"],
+                            transaction_hash=trade["transaction"]["transaction_hash"],
+                            event_type=trade["event_type"]
+                        )
+                if response.status_code == status.HTTP_200_OK and len(json) == 0:
+                    # we've exhausted the API history
+                    break
+                time.sleep(0.6) #sleep to prevent throttling.
+                i += 1
+            data.append(e)
             serializer = EventSerializer(data, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response("An error occurred.", status=status.HTTP_404_NOT_FOUND)
